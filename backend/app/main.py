c378ddd1dee4aa8b7662f8d67dc167f0bb710e95
goldmonkeypunk <andreykorzha04@gmail.com>
2025-06-11 10:07:40 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,18 +6,19 @@ from app.api.endpoints.auth import router as auth_router
 from app.api.endpoints.chords import router as chords_router
 from app.api.endpoints.songs import router as songs_router
 from app.api.endpoints.users import router as users_router
-from app.api.endpoints.chord_save import router as save_router
 from app.api.endpoints.chord_save import router as chord_save_router
-app = FastAPI(title="Гітарні акорди та пісні", version="1.0.0")    
+
+app = FastAPI(title="Гітарні акорди та пісні", version="1.0.0")
 
 
 @app.on_event("startup")
 def _startup():
     init_db()
-router.include_router(save_router)
-app.include_router(users_router)
+
 app.include_router(auth_router)
 app.include_router(chords_router)
 app.include_router(songs_router)
-app.include_router(chord_save_router)
+app.include_router(users_router)
+app.include_router(chord_save_router)   
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
