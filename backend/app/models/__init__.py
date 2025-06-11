0b864a2be8c6117c6ee40c428dd42e71757e34aa
goldmonkeypunk <andreykorzha04@gmail.com>
2025-06-11 04:26:21 +0300

    Update __init__.py

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,1 +1,14 @@
+from .base import Base
+from .user import User, UserRole
+from .chord import Chord
+from .song import Song, SongChord
+
+__all__ = [
+    "Base",
+    "User",
+    "UserRole",
+    "Chord",
+    "Song",
+    "SongChord",
+]
 
