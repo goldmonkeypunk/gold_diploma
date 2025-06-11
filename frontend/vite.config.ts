e6de69c221fcb884c94a6d3aa646b65da5175089
goldmonkeypunk <andreykorzha04@gmail.com>
2025-06-11 13:21:22 +0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,4 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
-
-export default defineConfig({
-  plugins: [react()],
-  server: { port: 5173 }
-})
+export default defineConfig({ plugins: [react()] })
 
