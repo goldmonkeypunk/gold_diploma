52f2f30035bf406d41aed678f96df93cc274cf3a
goldmonkeypunk <andreykorzha04@gmail.com>
2025-06-11 21:02:06 +0300

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,43 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import "./index.css";
-import "./i18n";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+async function bootstrap() {
+  try {
+    const [{ default: i18n }, { initReactI18next }] = await Promise.all([
+      import("i18next"),
+      import("react-i18next")
+    ]);
+
+    const uk = (await import("./locales/uk.json")).default;
+
+    await i18n.use(initReactI18next).init({
+      resources: { uk: { translation: uk } },
+      lng: "uk",
+      fallbackLng: "uk",
+      interpolation: { escapeValue: false }
+    });
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.warn("i18n not initialised:", e);
+  }
+
+  const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+  );
+
+  root.render(
+    <React.StrictMode>
+      <Suspense fallback={<p>Loading…</p>}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Suspense>
+    </React.StrictMode>
+  );
+}
+
+bootstrap();
